98ce3e0a4253babcff7799db638db498bdd04b34
EReyes13 <itsmeelio13@gmail.com>
2024-10-16 13:26:55 -0400

    The "first" version of the game.

---
+++ Assets/Assets/Scripts/HazardMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardMove : MonoBehaviour
+{
+    public bool MoveUp = true;
+    // Update is called once per frame
+    void Update()
+    {
+       if(MoveUp == true) {
+
+            transform.position -= new Vector3(0, 5, 0) * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += new Vector3(0, 5, 0) * Time.deltaTime;
+
+        }
+       if(transform.position.y >= 5)
+        {
+            MoveUp = true;
+
+        }
+       if(transform.position.y <= -5)
+        {
+            MoveUp = false;
+
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/PoroScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoroScript : MonoBehaviour
+{
+  
+    public void Fast()
+    {
+        Destroy(gameObject);
+    }
+}
